9dfde0b176fe42dd9bc856dd7f51a6cbe3a0a8e3
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-09-03 14:33:09 -0400

    ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Fantasy Commissions</h1>
+      <h1 className="title">Crimson Vial Commissions</h1>
       <CommissionForm />
     </div>
   );
